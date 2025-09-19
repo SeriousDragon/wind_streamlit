2ef0c73b81a236660e037ed16e7c3459eeaca40f
SeriousDragon <emart9743@gmail.com>
2025-09-19 15:39:28 +0300

    Update wind.py

--- wind.py
+++ wind.py
@@ -6,7 +6,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
-
+import torch
 from ultralytics import YOLO
 
 # # ---------- НАСТРОЙКИ СТРАНИЦЫ ----------
